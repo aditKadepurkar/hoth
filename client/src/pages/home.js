37f3b10c27c37fba16a43a4ee2c03222d3f43066
aditKadepurkar <adit.kadepurkar@gmail.com>
2024-03-03 17:12:30 -0600

    Structure homejs

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,8 +5,9 @@ export function Home() {
   return (
       <div className="w-screen h-screen bg-gray-900">
         <div className="w-1/3 h-screen bg-white rounded-r-[20%]">
-          
-  
+          <div>
+            
+          </div>
         </div>
   
   
