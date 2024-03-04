8d067fc737a1c8e4b658cd24e592d6f91e6319f8
aditKadepurkar <adit.kadepurkar@gmail.com>
2024-03-03 18:09:16 -0600

    saving

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Login } from "../components/LoginModal";
 
-export function Home() {
+// async function getData() {
+//   const res = await fetch("http://0.0.0.0:7000/");
+//   const data = JSON.parse(await res.json());
+//   return data;
+// }
 
+export async function Home() {
+  // const user = await getData()
   return (
       <div className="w-screen h-screen bg-gray-900">
         <div className="w-1/3 h-screen bg-white rounded-r-[20%]">
+        <Login/>
+          <div className="">
+            {/* PFP */}
+          </div>
           <div>
-            
+            {/* User DATA */}
           </div>
         </div>
   
